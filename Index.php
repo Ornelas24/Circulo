12f89f8d5d7e51d23c79259c81c75bcd296961cd
EmanuelLop <utp0009948@alumno.utpuebla.edu.mx>
2022-06-06 16:32:03 -0500

    Segundo commit: Exhibe el círculo

---
+++ Index.php
@@ -0,0 +1,11 @@
+<?php
+    header("Content-Type: image/png");
+    //require_once("Fraccion.php");
+    //$frac = new Fraccion(10,31);
+    //echo "La fracción es: ";
+    //$frac->exhibir();
+
+    require_once("Circulo.php");
+    $cir = new Circulo(200, 200,200, 400);
+    $cir->dibujar();
+?> 
